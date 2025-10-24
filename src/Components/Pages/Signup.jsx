444ce83d9a6bdeb06429eeda3e110d9e980b6496
Ramuthu Theniya <tramuthu@gmail.com>
2025-10-24 23:01:27 +0530

    Changes of sign up page

--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -2,44 +2,49 @@ import React from "react";
 import "../Styles/Signup.css";
 
 const Signup = () => {
-  return (
-    <div className="signup-page">
-      <header className="header">
-        <div className="logo">
-          <span>Tech Care</span>
-        </div>
-        <div className="nav">
-          <a href="#">Contact us</a>
-          <button className="login-btn">Login</button>
-        </div>
-      </header>
+return (
+<div className="signup-page">
+<header className="header">
+<div className="logo">
+<img src="\assets\logo.png" alt="Logo" className="left-logo" />
+<span>Tech Care</span>
+</div>
 
-      <main className="form-section">
-        <h1>
-          Tech Care <span>- IT Helpdesk Ticketing System</span>
-        </h1>
-        <p>
-          Streamlining Campus IT Support — From Problem to Resolution in Just a
-          Few Clicks.
-        </p>
+    <nav className="nav">
+      <a href="#">Contact us</a>
+      <button className="login-btn">Login</button>
+    </nav>
+  </header>
 
-        <form className="signup-form">
-          <input type="text" placeholder="Full Name*" />
-          <input type="email" placeholder="Work Email*" />
-          <input type="text" placeholder="Faculty Name*" />
-          <input type="tel" placeholder="Phone Number*" />
-          <button type="submit" className="signup-btn">
-            Sign up
-          </button>
-        </form>
+  <h1>
+      Tech Care <span>- IT Helpdesk Ticketing System</span>
+    </h1>
+    <p>
+      Streamlining Campus IT Support — From Problem to Resolution in Just a
+      Few Clicks.
+    </p>
 
-        <p className="policy">
-          By submitting this form, you agree to receive relevant updates and
-          service communications from Tech Care subject to our Privacy Policy.
-        </p>
-      </main>
-    </div>
-  );
+  <main className="form-section">
+    
+    <form className="signup-form">
+      <input type="text" placeholder="Full Name*" />
+      <input type="email" placeholder="Work Email*" />
+      <input type="text" placeholder="Faculty Name*" />
+      <input type="tel" placeholder="Phone Number*" />
+      <button type="submit" className="signup-btn">
+        Sign up
+      </button>
+    </form>
+  </main>
+
+  <p className="policy">
+      By submitting this form, you agree to receive relevant updates and
+      service communications from Tech Care subject to our Privacy Policy.
+    </p>
+</div>
+
+
+);
 };
 
 export default Signup;
